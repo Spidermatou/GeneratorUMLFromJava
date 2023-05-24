a935aa2e32b3506279d377904964b27369c4db93
Mattéo Gillig <matteo.gillig@etu.unistra.fr>
2023-05-24 16:06:09 +0200

    Ajout d'une méthode subParametre et modification de la classe Constructeurs

--- src/pumlFromJava/classes/Constructeurs.java
+++ src/pumlFromJava/classes/Constructeurs.java
@@ -3,6 +3,9 @@ package pumlFromJava.classes;
 import pumlFromJava.PumlDiagram;
 
 import javax.lang.model.element.*;
+import javax.lang.model.util.ElementFilter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Constructeurs
 {
@@ -32,8 +35,13 @@ public class Constructeurs
         //TypeElement ede=(TypeElement) e.getEnclosingElement();
 
         //Parametre
-        //mais il n'y a pas encore le nom du paramêtre
-         constructeur+=enleverVoidALaFin(petitElement.asType().toString())+"\n";
+        //mais il n'y a pas encore le nom du
+        List<String> fin = subStrParametre( petitElement.asType().toString());
+
+
+         constructeur+=enleverVoidALaFin( )+"\n";
+
+
 
          //Il faut encore améliorer le constructeur
 
@@ -53,5 +61,63 @@ public class Constructeurs
             System.out.println(enDehorsDesLimites);
             return texte;
         }
+    }
+
+    public static List<String> subStrParametre(String nom)
+    {
+        String retour = "";
+        List<String> listePara = new ArrayList<>();
+        int position=0;
+        int position2 = -5;
+        for(int i=0;i<nom.length();i++)
+        {
+            if(nom.charAt(i) == ',')
+            {
+                String retour2 = "";
+                String retour2inverse = "";
+                int j = position2;
+                while(nom.charAt(j) != '.')
+                {
+                    retour2 += nom.charAt(j);
+                    j--;
+                }
+
+            /*for(int k = retour2.length(); k == 0; k--)
+            {
+                retour2inverse += retour2.charAt(k);
+            }*/
+                int k = retour2.length()-1;
+                while(k >= 0)
+                {
+                    retour2inverse += retour2.charAt(k);
+                    k--;
+                }
+                listePara.add(retour2inverse);
+            }
+            if(nom.charAt(i)=='.')
+            {
+                position=i;
+            }
+        }
+
+
+        if(position==0) {
+            retour = nom;
+        }
+        else  {
+            listePara.add(nom.substring(position + 1));
+        }
+
+
+        //Dans certains cas, les noms sont entre < > (dans des listes, init, etc ...), et comme on garde tout après le dernier point, ce caractère peut rester
+        //On va alors simplement l'enlever s'il y a ce caractère
+        if(listePara.get(listePara.size()-1).charAt(listePara.get(listePara.size()-1).length()-1)=='>') {
+            String nouvMot;
+            nouvMot = listePara.get(listePara.size() - 1).substring(0, listePara.get(listePara.size() - 1).length() - 1);
+
+            listePara.remove(listePara.get(listePara.size()-1));
+            listePara.add(nouvMot);
+        }
+        return listePara;
     }
 }
